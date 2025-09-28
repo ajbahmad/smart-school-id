309f4cd0e81f3b71dfb6506498dd2492c69f72da
niysnsty <aixzniaa@gmail.com>
2025-09-28 15:50:05 +0700

    koneksi

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$database = "nama_database";
+
+// Membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+echo "Koneksi berhasil";
